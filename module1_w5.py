93ee188b0cdf636071904982212aa10721fcce44
reneenie <niel@purdue.edu>
2019-05-14 23:41:58 -0600

    finish w5

--- module1_w5.py
+++ module1_w5.py
@@ -4,3 +4,19 @@
 # If the user enters anything other than a valid number
 # catch it with a try/except and put out an appropriate message and ignore the number.
 # Enter 7, 2, bob, 10, and 4 and match the output below.
+
+while True:
+    tmp = input("Enter an integer: ")
+    if tmp == "done":
+        break
+    try:
+        tmp = integer(tmp)
+    except:
+        print('Invalid input')
+        continue
+    if tmp > largest_so_far:
+        largest_so_far = tmp
+    if tmp < smallest_so_far:
+        smallest_so_far = tmp
+print('Maximum is',largest_so_far)
+print('Minimum is',smallest_so_far)
