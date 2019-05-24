3df8e84d487433fb5166fc04d925ba98c1c3b39c
reneenie <niel@purdue.edu>
2019-05-24 17:15:06 -0400

    create

---
+++ m2w1.py
@@ -0,0 +1,22 @@
+# changes from python2 to python3
+# 1. print '' -> print('')
+# 2. raw_input() -> input()
+# 3. how to handle complex charaxters
+
+# data structure 1: String
+# input() always gives you strings
+
+# SLICING begining and up to but not including
+word = 'banana'
+print(word[2:4])
+print(word[:5])
+print(word[2:])
+
+# String comparison
+'A' < 'a' # true
+'z' > 'a' # return true
+'Z' > 'A' # return true
+
+# functions in string library
+greet = 'Hi'
+print(greet.lower())
